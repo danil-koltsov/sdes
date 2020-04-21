da581946021985f005861e01d6307664087102a4
danil-koltsov <62597478+danil-koltsov@users.noreply.github.com>
2020-04-21 15:09:23 +0300

    Update sdes.sh

--- sdes.sh
+++ sdes.sh
@@ -21,8 +21,8 @@ for ((j=0; j<=$binLength; j++)); do
 done
 }
 function sBox {
-local txtSBox=($1); shift
-local sBoxS=($1)
+txtSBox=($1); shift
+sBoxS=($1)
 case "$sBoxS" in
     sBoxS0)
         declare -A sBoxSVersion=([0,0]=1 [0,1]=0 [0,2]=3 [0,3]=2
@@ -54,22 +54,22 @@ function keyGeneration1 {
 keyIn=($1)
 keyP10=($(permutation "${p10[*]}" "${keyIn[*]}")) #p10
 keyL=(${keyP10[@]:0:5}); keyR=(${keyP10[@]:5:10}) #Division
-ls1L=(${keyL[@]:1:4}); ls1R+=(${keyR[@]:1:4}) #<
+ls1L=(${keyL[@]:1:4}); ls1R+=(${keyR[@]:1:4}) #Offset <
 ls1L[5]=${keyL[0]}; ls1R[5]=${keyR[0]}
 ls1=(${ls1L[@]}); ls1+=(${ls1R[@]})
-echo $(permutation "${p8[*]}" "${ls1[*]}")
+echo $(permutation "${p8[*]}" "${ls1[*]}") #p8
 }
 function keyGeneration2 {
 keyIn=($1)
 keyP10=($(permutation "${p10[*]}" "${keyIn[*]}")) #p10
 keyL=(${keyP10[@]:0:5}); keyR=(${keyP10[@]:5:10}) #Division
-ls1L=(${keyL[@]:1:4}); ls1R+=(${keyR[@]:1:4}) #<
+ls1L=(${keyL[@]:1:4}); ls1R+=(${keyR[@]:1:4}) #Offset <
 ls1L[5]=${keyL[0]}; ls1R[5]=${keyR[0]}
 ls1=(${ls1L[@]}); ls1+=(${ls1R[@]})
-ls2L=(${ls1L[@]:2:4}); ls2R=(${ls1R[@]:2:4}) #<<
+ls2L=(${ls1L[@]:2:4}); ls2R=(${ls1R[@]:2:4}) #Offset <<
 ls2L+=(${ls1L[@]:0:2}); ls2R+=(${ls1R[@]:0:2})
 ls2=(${ls2L[@]}); ls2+=(${ls2R[@]})
-echo $(permutation "${p8[*]}" "${ls2[*]}")
+echo $(permutation "${p8[*]}" "${ls2[*]}") #p8
 }
 function round {
 txtRoundInput=($1); shift
@@ -90,9 +90,9 @@ keyInput=($1)
 key1=($(keyGeneration1 "${keyInput[*]}")) #KeyGen 1
 key2=($(keyGeneration2 "${keyInput[*]}")) #KeyGen 2
 txtIp=($(permutation "${ip[*]}" "${txtInput[*]}")) #Ip
-txtRound=($(round "${txtIp[*]}" "${key1[*]}")) #Round 1
-txtRoundRotate=(${txtRound[@]:4:8}); txtRoundRotate+=(${txtRound[@]:0:4}) #Rotate
-txtRound=($(round "${txtRoundRotate[*]}" "${key2[*]}")) #Round 2
+txtRound=($(round "${txtIp[*]}" "${key1[*]}")) #fK1
+txtRoundRotate=(${txtRound[@]:4:8}); txtRoundRotate+=(${txtRound[@]:0:4}) #P
+txtRound=($(round "${txtRoundRotate[*]}" "${key2[*]}")) #fK2
 echo $(permutation "${ip1[*]}" "${txtRound[*]}") #Ip-1
 }
 function decryption {
@@ -101,19 +101,26 @@ keyInput=($1)
 key1=($(keyGeneration1 "${keyInput[*]}")) #KeyGen 1
 key2=($(keyGeneration2 "${keyInput[*]}")) #KeyGen 2
 txtIp=($(permutation "${ip[*]}" "${txtInput[*]}")) #Ip
-txtRound=($(round "${txtIp[*]}" "${key2[*]}")) #Round 1
-txtRoundRotate=(${txtRound[@]:4:8}); txtRoundRotate+=(${txtRound[@]:0:4}) #Rotate
-txtRound=($(round "${txtRoundRotate[*]}" "${key1[*]}")) #Round 2
+txtRound=($(round "${txtIp[*]}" "${key2[*]}")) #fK1
+txtRoundRotate=(${txtRound[@]:4:8}); txtRoundRotate+=(${txtRound[@]:0:4}) #P
+txtRound=($(round "${txtRoundRotate[*]}" "${key1[*]}")) #fK2
 echo $(permutation "${ip1[*]}" "${txtRound[*]}") #Ip-1
 }
-txt=(0 1 1 0 1 1 0 1)
-key=(0 0 1 0 0 1 0 1 1 1)
-echo txt: ${txt[@]}
-echo key: ${key[@]}
-txtCipher=$(encryption "${txt[*]}" "${key[*]}")
-echo Cipher txt: ${txtCipher[@]}
-txtDecrypted=$(decryption "${txtCipher[*]}" "${key[*]}")
-echo Decrypted txt: ${txtDecrypted[@]}
+if [[ $# < 3 ]] || [[ "$1" != "-e" && "$1" != "-d" ]]; then
+    echo "Usage: -e|-d \"txt[8 bits]\" \"key[10 bits]\""
+    exit 0
+fi
+func=("$1"); shift
+txt=("$1"); shift
+key=("$1")
+case "${func}" in
+    "-e")
+        txtCipher=$(encryption "${txt[*]}" "${key[*]}")
+        echo Cipher txt: ${txtCipher[@]};;
+    "-d")
+        txtDecrypted=$(decryption "${txt[*]}" "${key[*]}")
+        echo Decrypted txt: ${txtDecrypted[@]};;
+esac
 #  sdes.sh
 #
 #
